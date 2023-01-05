08e41861a0bb1f58c2d74d8574909a4a7019dc11
LidorLazar <101813209+LidorLazar@users.noreply.github.com>
2023-01-05 17:46:42 +0200

    Delete logic.py
    no need this file more

--- Linkedin/logic.py
+++
@@ -1,9 +0,0 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-def open_web():
-    driver = webdriver.Chrome('./chromedriver.exe')
-    url = 'https://www.linkedin.com/'
-    driver.get(url)
-
